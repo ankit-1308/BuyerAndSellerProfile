04c01d9c5952d8c9ccc62e7a9265256160a82798
Ankit Maurya <ankit.maurya@olx.in>
2025-02-01 14:17:38 +0530

    create a card

---
+++ BuyerAndSellerProfile/StatsView.swift
@@ -0,0 +1,20 @@
+//
+//  StatsView.swift
+//  BuyerAndSellerProfile
+//
+//  Created by Ankit Maurya  on 01/02/25.
+//
+
+import UIKit
+
+class StatsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
